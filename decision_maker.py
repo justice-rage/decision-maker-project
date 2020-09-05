f7a5f2def9c9fbbf0ce78660f1d4e3a59b98541c
Justice Peltier <peltierjustice@gmail.com>
2020-09-05 19:44:39 -0400

    Typo correction, line 10

--- decision_maker.py
+++ decision_maker.py
@@ -7,6 +7,6 @@ option_1 = str(input("Enter option 1: "))
 option_2 = str(input("Enter option 2: "))
 
 options = [option_1, option_2]
-decision = random.Mchoice(options)
+decision = random.choice(options)
 
 print(decision)
