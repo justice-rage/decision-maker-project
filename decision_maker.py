bbe4e68ffa5e6fdc46fe5ba47b28bc749a309c89
Justice Peltier <peltierjustice@gmail.com>
2020-09-05 19:42:25 -0400

    Initial commit, first iteration
    Foundation code established

---
+++ decision_maker.py
@@ -0,0 +1,12 @@
+# Decision Maker Project
+# Takes user inputs and randomly returns one input
+
+import random
+
+option_1 = str(input("Enter option 1: "))
+option_2 = str(input("Enter option 2: "))
+
+options = [option_1, option_2]
+decision = random.Mchoice(options)
+
+print(decision)
