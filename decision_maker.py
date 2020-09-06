195ca24c1da367291238afe3536b7ac44ff2bc84
Justice Peltier <peltierjustice@gmail.com>
2020-09-06 09:30:48 -0400

    Update, improved functionality
    Changes:
    1) Added list to store # of options via user input.
    2) Added for loop that prompts user to input options
    3) Utilized random.choice() to return random option from list.

--- decision_maker.py
+++ decision_maker.py
@@ -3,10 +3,20 @@
 
 import random
 
-option_1 = str(input("Enter option 1: "))
-option_2 = str(input("Enter option 2: "))
+# List of options
+option_list = []
 
-options = [option_1, option_2]
-decision = random.choice(options)
+# Number of options
+option_number = int(input("Enter number of options: "))
+print()
 
-print(decision)
+# Iterates through range
+for i in range(0, option_number):
+    option = str(input(f"Enter choice {i + 1}: "))
+
+    option_list.append(option) # Adds submitted options to list
+
+decision = random.choice(option_list) # Randomly returns one option
+
+print()
+print(f"Decision: {decision}")
